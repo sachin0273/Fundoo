137d120d5ff4828131b859f9b84ef43e364ade9d
sachin0273 <sachinjadhav0273@gmail.com>
2019-11-20 14:32:10 +0530

    updated gitignore for ignore scannerwork and load testing functionality added in locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,30 +1,84 @@
+import json
+
+from django.urls import reverse
 from locust import HttpLocust, TaskSet
+
+header = {"Content-Type": "application/json",
+          "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+                           ".eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc0MzE4MzIxLCJqdGkiOiJlMzFmNjcxZTE5ODY0Mzk5YWY0ZTFkNmYzYWU0YWFlZiIsInVzZXJfaWQiOjF9.ihPvdHILOZvkc2YTuTUhIVP7SEzC7Ryj7n66iWb0kSU"}
 
 
 def login(l):
-    l.client.post("login/", {"username": "admin", "password": "admin123"})
+    l.client.post("/login/", {"username": "admin", "password": "admin123"})
 
 
-# def logout(l):
-#     l.client.post("/logout/", {"username": "jadhav", "password": "jadhav"})
+def logout(l):
+    l.client.get("/logout/", headers=header)
 
 
-def index(l):
-    l.client.get("/")
+def reset_password(l):
+    l.client.post("/sendemail/", {"email": "sachinjadhav0273@gmail.com"})
 
 
-def profile(l):
-    l.client.get('/hello/')
+def note_put(l):
+    l.client.put("/api/note/9",
+                 json.dumps({"title": "stringjkjk", "note": "stringhh", "collaborator": [], "label": []}),
+                 headers=header)
+
+
+def note_post(l):
+    l.client.post("/api/note/", json.dumps({
+        "title": "string",
+        "note": "string",
+        "image": "string",
+        "is_archive": True,
+        "is_pin": True,
+        "collaborator": [
+
+        ],
+        "label": [
+
+        ]
+    }), headers=header)
+
+
+def note_get(l):
+    l.client.get("/api/note/", headers=header)
+
+
+def label_post(l):
+    l.client.post("/api/label/", json.dumps({"name": "jkll"}), headers=header)
+
+
+def label_get(l):
+    l.client.get("/api/label/", headers=header)
+
+
+def label_put(l):
+    l.client.put("/api/label/25", json.dumps({"name": "ty"}), headers=header)
+
+
+def get_archive_note(l):
+    l.client.get("/api/note/archive/", headers=header)
+
+
+def get_trash_note(l):
+    l.client.get("/api/note/trash/", headers=header)
+
+
+def get_reminder_note(l):
+    l.client.get("/api/note/reminder/", headers=header)
 
 
 class UserBehavior(TaskSet):
-    tasks = {index: 2, profile: 1}
+    tasks = {reset_password: 1, note_post: 1, note_get: 1, note_put: 1, label_post: 1,
+             label_get: 1, label_put: 1, get_archive_note: 1, get_trash_note: 1, get_reminder_note: 1}
 
     def on_start(self):
         login(self)
 
     def on_stop(self):
-        profile(self)
+        logout(self)
 
 
 class WebsiteUser(HttpLocust):
